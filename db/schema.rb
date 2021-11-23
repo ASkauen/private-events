08e6d70798e8547dbef381afc9364136c194a870
ASkauen <andreas@rubynor.com>
2021-11-23 11:43:25 +0100

    Add user_id ref to event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_205755) do
+ActiveRecord::Schema.define(version: 2021_11_23_084713) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_11_20_205755) do
     t.string "title"
     t.string "desc"
     t.string "date"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_11_20_205755) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
