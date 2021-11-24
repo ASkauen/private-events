b8f0bca3eaad960629582f4ec0eddc88b569bb47
ASkauen <andreas@rubynor.com>
2021-11-24 09:18:30 +0100

    Add username field to sign in/up. Make devise authenticate with username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :events, inverse_of: :creator
 
-  validates :username, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 end
 
 # == Schema Information
