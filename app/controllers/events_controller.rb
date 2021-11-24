0fa39acba234cd0443d65cf21f5dc34143052606
ASkauen <andreas@rubynor.com>
2021-11-24 08:59:51 +0100

    Add new event button and form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @events = Event.all
   end
@@ -8,5 +9,23 @@ class EventsController < ApplicationController
   def show
     puts params[:id]
     @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :desc, :date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  resources :events, only: %i[show index]
+  resources :events
   resources :users, only: [:show]
   root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
