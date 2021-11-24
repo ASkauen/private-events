02288e95e3c08dad9bccedfe77c43ba7a2da9198
ASkauen <andreas@rubynor.com>
2021-11-24 11:38:01 +0100

    Add new method

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,11 @@
 class AttendancesController < ApplicationController
+  def new
+    @attendance = current_user.attendances.build(attendance_params)
+  end
+
+  private
+  def attendance_params
+    params.require(:event).permit(:id)
+  end
+
 end
